89f334f8864230642a7f4d2988f907e5a118cc0f
Thomas Lacroix <t.f.m.lacroix@gmail.com>
2021-03-30 17:19:30 +0200

    :sparkles: added default controls to video player

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,213 +1,216 @@
- <template>
-   <div class="home-container">
-     <Canvas/>
-     <Loader :data.sync="this.loader" @ended="hasLoaded" />
-     <div v-if="markers" class="container">
-       <Header />
-       <div v-for="(marker, index) of markers" v-bind:key="marker.id" class="markers" ref="markers"
-         @click="goToArticle(slug_post[index])">
-         <div class="img-container" v-if="marker['image']['sizes']['medium_large']">
-           <img :src="marker['image']['sizes']['medium_large']" />
-         </div>
-       </div>
-     </div>
-   </div>
- </template>
- <script>
-   import Header from '@/components/partials/Header'
-   import Loader from '@/components/partials/Loader'
-   import Canvas from '@/components/partials/Canvas'
+<template>
+  <div class="home-container">
+    <Loader :data.sync="this.loader" @ended="hasLoaded" />
+    <div v-if="markers" class="container">
+      <Header />
+      <div
+        v-for="(marker, index) of markers"
+        v-bind:key="marker.id"
+        class="markers"
+        ref="markers"
+        @click="goToArticle(slug_post[index])"
+      >
+        <div
+          class="img-container"
+          v-if="marker['image']['sizes']['medium_large']"
+        >
+          <img :src="marker['image']['sizes']['medium_large']" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Header from "@/components/partials/Header";
+import Loader from "@/components/partials/Loader";
+import Canvas from "@/components/partials/Canvas";
 
-   export default {
-     name: 'Home',
-     data() {
-       return {
-         markers: [],
-         slug_post: [],
-         loader: '',
-       }
-     },
-     mounted() {
-       window.addEventListener('resize', () => {
-         this.$refs.markers.forEach(element => {
-           if (element)
-             element.style.height = element.offsetWidth + "px";
-         });
-       })
+export default {
+  name: "Home",
+  data() {
+    return {
+      markers: [],
+      slug_post: [],
+      loader: ""
+    };
+  },
+  mounted() {
+    window.addEventListener("resize", () => {
+      this.$refs.markers.forEach(element => {
+        if (element) element.style.height = element.offsetWidth + "px";
+      });
+    });
 
-       fetch('https://www.morganelapisardi.fr/backoffice/index.php/wp-json/markers/v1/post')
-         .then((r) => r.json())
-         .then((res) => {
-           this.markers = res.map(x => x.acf)
-           this.slug_post = res.map(x => x.slug)
-           let number_image_loaded = 0;
-           this.loader = 0.5
-           this.markers.forEach((marker, index) => {
-             let objImg = new Image();
-             objImg.src = marker['image']['sizes']['medium_large'];
-             objImg.onload = () => {
-               number_image_loaded++
-               this.loader = ((number_image_loaded / this.markers.length) / 2) + 0.5
-             }
-           });
-
-
-         })
-         .catch(error => console.log('error is', error));
-     },
-     activated() {
-       if(this.$refs.markers) {
+    fetch(
+      "https://www.morganelapisardi.fr/backoffice/index.php/wp-json/markers/v1/post"
+    )
+      .then(r => r.json())
+      .then(res => {
+        this.markers = res.map(x => x.acf);
+        this.slug_post = res.map(x => x.slug);
+        let number_image_loaded = 0;
+        this.loader = 0.5;
+        this.markers.forEach((marker, index) => {
+          let objImg = new Image();
+          objImg.src = marker["image"]["sizes"]["medium_large"];
+          objImg.onload = () => {
+            number_image_loaded++;
+            this.loader = number_image_loaded / this.markers.length / 2 + 0.5;
+          };
+        });
+      })
+      .catch(error => console.log("error is", error));
+  },
+  activated() {
+    if (this.$refs.markers) {
+      this.$refs.markers.forEach(element => {
+        element.style.height = element.offsetWidth + "px";
+      });
+    }
+  },
+  methods: {
+    goTodetail(prodId) {
+      this.$router.push({
+        name: "blog",
+        params: {
+          Pid: prodId,
+          home: "home"
+        }
+      });
+    },
+    goToArticle(slug) {
+      this.$router.push({
+        name: "blog",
+        params: {
+          Pid: slug,
+          home: "Home"
+        }
+      });
+    },
+    hasLoaded(value) {
+      let interval = setInterval(() => {
         this.$refs.markers.forEach(element => {
           element.style.height = element.offsetWidth + "px";
+          if (element.offsetHeight == element.offsetWidth) {
+            setTimeout(() => {
+              clearInterval(interval);
+            }, 10000);
+          }
         });
-       }
-     },
-     methods: {
-       goTodetail(prodId) {
-         this.$router.push({
-           name: 'blog',
-           params: {
-             Pid: prodId,
-             home: "home"
-           }
-         })
-       },
-       goToArticle(slug) {
-         this.$router.push({
-           name: 'blog',
-           params: {
-             Pid: slug,
-             home: "Home"
-           }
-         })
-       },
-       hasLoaded(value) {
-         let interval = setInterval(() => {
-           this.$refs.markers.forEach(element => {
-             element.style.height = element.offsetWidth + "px";
-             if (element.offsetHeight == element.offsetWidth) {
-               setTimeout(() => {
-                 clearInterval(interval)
-               }, 10000);
-             }
-           });
-         }, 200);
-       }
-     },
-     components: {
-       'Header': Header,
-       'Loader': Loader,
-       'Canvas': Canvas,
-     }
-   }
- </script>
+      }, 200);
+    }
+  },
+  components: {
+    Header: Header,
+    Loader: Loader,
+    Canvas: Canvas
+  }
+};
+</script>
 
- <style scoped>
+<style scoped>
+.image_logo_red {
+  position: absolute;
+  width: 50px;
+  height: auto;
+  bottom: 30px;
+}
 
-   .image_logo_red {
-     position: absolute;
-     width: 50px;
-     height: auto;
-     bottom: 30px;
-   }
+#canvas {
+  mix-blend-mode: hard-light !important;
+}
 
-   #canvas {
-     mix-blend-mode: hard-light !important;
-   }
+@keyframes left {
+  0% {
+    transform: scaleX(0);
+    transform-origin: left;
+  }
 
+  49% {
+    transform: scaleX(1);
+    transform-origin: left;
+  }
 
-   @keyframes left {
-     0% {
-       transform: scaleX(0);
-       transform-origin: left;
-     }
+  51% {
+    transform: scaleX(1);
+    transform-origin: right;
+  }
 
-     49% {
-       transform: scaleX(1);
-       transform-origin: left;
-     }
+  100% {
+    transform: scaleX(0);
+    transform-origin: right;
+  }
+}
 
-     51% {
-       transform: scaleX(1);
-       transform-origin: right;
-     }
+.home-container {
+  margin: 5px;
+}
 
-     100% {
-       transform: scaleX(0);
-       transform-origin: right;
-     }
-   }
+.container {
+  display: flex;
+  flex-flow: row wrap;
+}
 
-   .home-container {
-     margin: 5px;
-   }
+.markers {
+  width: 20%;
+  height: 300px;
+  cursor: pointer;
+  padding: 5px;
+  box-sizing: border-box;
+  user-select: none;
+}
 
-   .container {
-     display: flex;
-     flex-flow: row wrap;
-   }
+.markers:hover img {
+  transform: scale(1.01);
+}
 
-   .markers {
-     width: 20%;
-     height: 300px;
-     cursor: pointer;
-     padding: 5px;
-     box-sizing: border-box;
-     user-select: none;
-   }
+.markers_inner {
+  height: 100%;
+}
 
-   .markers:hover img {
-     transform: scale(1.01);
-   }
+.logo {
+  width: 20%;
+  height: 300px;
+  cursor: pointer;
+  margin: 10px;
+  background: rebeccapurple;
+}
 
-   .markers_inner {
-     height: 100%;
-   }
+.img-container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
 
-   .logo {
-     width: 20%;
-     height: 300px;
-     cursor: pointer;
-     margin: 10px;
-     background: rebeccapurple;
-   }
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transform-origin: center;
+  transition: 0.3s ease-in-out all;
+}
 
-   .img-container {
-     width: 100%;
-     height: 100%;
-     overflow: hidden;
-   }
+@media only screen and (max-width: 1600px) {
+  .markers {
+    width: 25%;
+  }
+}
 
-   img {
-     width: 100%;
-     height: 100%;
-     object-fit: cover;
-     transform-origin: center;
-     transition: 0.3s ease-in-out all;
-   }
+@media only screen and (max-width: 1400px) {
+  .markers {
+    width: 33%;
+  }
+}
 
-   @media only screen and (max-width: 1600px) {
-     .markers {
-       width: 25%;
-     }
-   }
+@media only screen and (max-width: 1000px) {
+  .markers {
+    width: 50%;
+  }
+}
 
-   @media only screen and (max-width: 1400px) {
-     .markers {
-       width: 33%;
-     }
-   }
-
-   @media only screen and (max-width: 1000px) {
-     .markers {
-       width: 50%;
-     }
-   }
-
-   @media only screen and (max-width: 600px) {
-     .markers {
-       width: 100%;
-     }
-   }
-
- </style>
+@media only screen and (max-width: 600px) {
+  .markers {
+    width: 100%;
+  }
+}
+</style>
--- src/components/blocs/Video.vue
+++ src/components/blocs/Video.vue
@@ -1,90 +1,92 @@
 <template>
   <div class="video-container">
-      <video class="video__player" :src="data['video'].url" muted autoplay loop></video>
-      <div v-if="data['show_seekbar']" class="seek_bar"></div>
-      <span>{{ data['legend'] }}</span>
+    <video
+      class="video__player"
+      :src="data['video'].url"
+      muted
+      autoplay
+      loop
+      controls
+    ></video>
+    <div v-if="data['show_seekbar']" class="seek_bar"></div>
+    <span>{{ data["legend"] }}</span>
   </div>
 </template>
 
 <script>
+export default {
+  name: "Video",
+  props: ["data_bloc"],
+  data() {
+    return {
+      data: this.data_bloc
+    };
+  },
+  mounted() {
+    let video = document.querySelector(".video__player");
+    let seekbar = {
+      value: 0,
+      el: document.querySelector(".seek_bar")
+    };
 
-  export default {
-    name: 'Video',
-    props: ['data_bloc'],
-    data() {
-      return {
-        data: this.data_bloc,
+    video.addEventListener("timeupdate", () => {
+      if (this.data["show_seekbar"]) {
+        seekbar.value =
+          Math.round((video.currentTime / video.duration) * 10000) / 10000;
+        seekbar.el.style.transform = `scaleX(${seekbar.value})`;
       }
-    },
-    mounted() {
-      let video = document.querySelector('.video__player')
-      let seekbar = {
-        value: 0,
-        el: document.querySelector('.seek_bar')
-      }
-
-      video.addEventListener('timeupdate', () => {
-        if(this.data['show_seekbar']) {
-          seekbar.value = Math.round(video.currentTime / video.duration * 10000)/10000
-          seekbar.el.style.transform = `scaleX(${seekbar.value})`
-        }
-      })
-
-    },
-    methods: {
-
-    }
-  }
-
+    });
+  },
+  methods: {}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- 
- .video-container {
-   width: 80%;
-   margin: 50px 10%;
-   box-sizing: border-box;
-   text-align: left;
-   overflow-x: hidden;
- }
+.video-container {
+  width: 80%;
+  margin: 50px 10%;
+  box-sizing: border-box;
+  text-align: left;
+  overflow-x: hidden;
+}
 
- .seek_bar {
-   width: 100%;
-   height: 5px;
-   background: #8A1538;
-   transform: scaleX(0);
-   transform-origin: left;
-   transition: 0.4s linear transform;
- }
+.seek_bar {
+  width: 100%;
+  height: 5px;
+  background: #8a1538;
+  display: none;
+  transform: scaleX(0);
+  transform-origin: left;
+  transition: 0.4s linear transform;
+}
 
- .video-container video{
-   width: 100%;
-   max-height: 80vh;
-   display: block;
-   background: black;
- }
+.video-container video {
+  width: 100%;
+  max-height: 80vh;
+  display: block;
+  background: black;
+}
 
- .video-container span{
-   display: block;
-   width: 100%;
-    font-weight: bold;
-    font-size: 1.1em;
-    padding: 10px 0;
-    box-sizing: border-box;
- }
+.video-container span {
+  display: block;
+  width: 100%;
+  font-weight: bold;
+  font-size: 1.1em;
+  padding: 10px 0;
+  box-sizing: border-box;
+}
 
-  @media only screen and (max-width: 700px) {
-    .galery__image {
-      width: 45%;
-      height: 300px;
-    }
+@media only screen and (max-width: 700px) {
+  .galery__image {
+    width: 45%;
+    height: 300px;
   }
+}
 
-  @media only screen and (max-width: 400px) {
-    .video-container span {
-      font-size: 0.8em;
-    }
+@media only screen and (max-width: 400px) {
+  .video-container span {
+    font-size: 0.8em;
   }
-
+}
 </style>
